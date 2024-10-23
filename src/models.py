26fa83af287ff33f43ebe3b9cc7ba82993770089
GuyFawkess <80330724+GuyFawkess@users.noreply.github.com>
2024-10-23 14:52:50 +0000

    empezando el proyecto

--- src/models.py
+++ src/models.py
@@ -1,11 +1,46 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50),unique=True, nullable=False)
+    fisrtname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(50), unique=True, nullable=False)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integrer)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250))
+    author_id = Column(Integer)
+    post_id = Column(Integer)
+
+class Media(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250))
+    post_id = Column(Integer)
+    type = Column(Enum('video', 'imagen'))
+
+
 
 class Person(Base):
     __tablename__ = 'person'
