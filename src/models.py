616084c860833311f8107a252b785af5fb2234ee
GuyFawkess <80330724+GuyFawkess@users.noreply.github.com>
2024-10-24 13:40:47 +0000

    primer trabajo echo en clase terminado

--- src/models.py
+++ src/models.py
@@ -12,33 +12,51 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50),unique=True, nullable=False)
-    fisrtname = Column(String(50))
+    firstname = Column(String(50))
     lastname = Column(String(50))
     email = Column(String(50), unique=True, nullable=False)
-
+    #realaciones
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='author')
 
 class Post(Base):
     __tablename__ = 'post'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integrer)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    #relaciones
+    user = relationship('User', back_populates='posts')
+    media = relationship('Media', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
 
 class Comment(Base):
     __tablename__ = 'comment'
 
     id = Column(Integer, primary_key=True)
     content = Column(String(250))
-    author_id = Column(Integer)
-    post_id = Column(Integer)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    #relaciones
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
 
 class Media(Base):
-    __tablename__ = 'comment'
+    __tablename__ = 'media'
 
     id = Column(Integer, primary_key=True)
     url = Column(String(250))
-    post_id = Column(Integer)
-    type = Column(Enum('video', 'imagen'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    type = Column(Enum('video', 'imagen', name='media_type'))
+
+    #realaciones
+    post = relationship('Post', back_populates='media')
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True,)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
 
 
